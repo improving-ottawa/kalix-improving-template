a0551df0f1503d50d911f64c7a6af09177e2c636
alexweinstein <alex.weinstein@improving.com>
2023-09-22 15:25:51 -0400

    feat: scalafmt [K2SO-20]

--- gateway/src/main/scala/com/improving/template/Main.scala
+++ gateway/src/main/scala/com/improving/template/Main.scala
@@ -17,8 +17,7 @@ object Main {
     // and is kept up-to-date with any changes in your protobuf definitions.
     // If you prefer, you may remove this and manually register these components in a
     // `Kalix()` instance.
-    KalixFactory.withComponents(
-      new GatewayAction(_))
+    KalixFactory.withComponents(new GatewayAction(_))
   }
 
   def main(args: Array[String]): Unit = {
