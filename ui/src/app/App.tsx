780bbd8add069a3ff027839af83bb044f9969b5f
alexweinstein <alex.weinstein@improving.com>
2023-12-04 08:22:36 -0500

    Template Signin (WIP)

--- ui/src/app/App.tsx
+++ ui/src/app/App.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./screens/Home";
 import {Provider} from "react-redux";
+import {createTheme, ThemeProvider} from "@mui/material";
 import {store} from "./redux/store";
+import {Login} from "@mui/icons-material";
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {LocalizationProvider} from '@mui/x-date-pickers';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#00488A',
+            contrastText: '#FFFFFF',
+        },
+        secondary: {
+            main: "#FCFEFF",
+            light: "#FCFEFF",
+            dark: "#FCFEFF",
+            contrastText: "#FCFEFF",
+        },
+    },
+});
 
 function App() {
     return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/">
-                        <Route index element={<Home/>}/>
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </Provider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <ThemeProvider theme={theme}>
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/">
+                                <Route index element={<Login/>}/>
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+                </Provider>
+            </ThemeProvider>
+        </LocalizationProvider>
     );
 }
 
