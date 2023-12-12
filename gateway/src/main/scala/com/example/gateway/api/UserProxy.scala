eb7048ac7fd89169fdc1e8111bcddeec5670e5d3
Kevin DiVincenzo <kevin.divincenzo@yoppworks.com>
2023-12-12 14:47:40 -0500

    Minor updates:   - Removed unused (query) parameter from "BeginAuthenticationRequest"   - Added "updateUserRole" to Gateway's gRPC API (authenticated and authorized Kalix Action)

--- gateway/src/main/scala/com/example/gateway/api/UserProxy.scala
+++ gateway/src/main/scala/com/example/gateway/api/UserProxy.scala
@@ -1,17 +1,25 @@
 package com.example.gateway.api
 
-import com.example.gateway.domain.{GetUserRequest, GetUserResponse}
+import com.example.gateway.domain._
 import com.example.gateway.middleware._
+
 import kalix.scalasdk.action.Action
 
-trait UserProxy extends GatewayProxyBase with AuthenticatedAction {
+trait UserProxy extends GatewayProxyBase with AuthenticatedAction with AuthorizedAction {
 
   override def getUser(request: GetUserRequest): Action.Effect[GetUserResponse] =
     authenticatedEffect { authToken =>
-      if (authToken.subject != request.userId)
+      if (authToken.subject != request.userId && !authToken.roles.contains("Admin"))
         effects.error("You are not authorized to perform this action.", io.grpc.Status.Code.PERMISSION_DENIED)
       else
         effects.forward(components.userEntity.getUser(request))
+    }
+
+  override def updateUserRoles(request: UpdateUserRolesRequest): Action.Effect[UserInfo] =
+    authenticatedEffect {
+      withRequiredRole("Admin") {
+        effects.forward(components.userEntity.updateUserRoles(request))
+      }
     }
 
 }
