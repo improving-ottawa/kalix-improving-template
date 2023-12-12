9f31f44fed6e5df80efb1716e6a5bc35666c8fa2
Kevin DiVincenzo <kevin.divincenzo@yoppworks.com>
2023-12-12 15:30:24 -0500

    Inverting `if` to simply check in "getUser"

--- gateway/src/main/scala/com/example/gateway/api/UserProxy.scala
+++ gateway/src/main/scala/com/example/gateway/api/UserProxy.scala
@@ -9,10 +9,10 @@ trait UserProxy extends GatewayProxyBase with AuthenticatedAction with Authorize
 
   override def getUser(request: GetUserRequest): Action.Effect[GetUserResponse] =
     authenticatedEffect { authToken =>
-      if (authToken.subject != request.userId || !authToken.roles.contains("Admin"))
-        effects.error("You are not authorized to perform this action.", io.grpc.Status.Code.PERMISSION_DENIED)
-      else
+      if (authToken.subject == request.userId || authToken.roles.contains("Admin"))
         effects.forward(components.userEntity.getUser(request))
+      else
+        effects.error("You are not authorized to perform this action.", io.grpc.Status.Code.PERMISSION_DENIED)
     }
 
   override def updateUserRoles(request: UpdateUserRolesRequest): Action.Effect[UserInfo] =
